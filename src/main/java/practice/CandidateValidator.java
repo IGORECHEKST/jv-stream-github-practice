9f63fdd497f4c8cf26d6ec9a2906d2b369b658e6
IGORECHEKST <165463152+IGORECHEKST@users.noreply.github.com>
2025-07-10 21:04:52 +0300

    my first sol

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,39 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() < 35) {
+            return false;
+        }
+
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+
+        if (!"Ukrainian".equalsIgnoreCase(candidate.getNationality())) {
+            return false;
+        }
+
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        if (years.length != 2) {
+            return false;
+        }
+        try {
+            int startYear = Integer.parseInt(years[0]);
+            int endYear = Integer.parseInt(years[1]);
+            if ((endYear - startYear) < 10) {
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
+    }
 }
+
